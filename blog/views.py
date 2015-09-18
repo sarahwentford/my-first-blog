2edd5a50c1bfcce580ef8de0eeee4ada9facdb3d
Sarah Wentford <sarah@example.com>
2015-09-18 15:09:10 +0100

    Comments added/removed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
