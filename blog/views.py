a2b9689af55194abd1e2043e3e896fecef2a5d39
Sarah Wentford <sarah@example.com>
2015-09-21 14:50:45 +0100

    New universe layout

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -40,7 +40,7 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
